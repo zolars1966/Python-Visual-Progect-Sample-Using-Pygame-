fe5fb7fd1da6cece15f0765c97476689e2a3ea1c
Arseny Zolotarev <70763346+ckreesher@users.noreply.github.com>
2022-01-07 00:05:23 +0800

    Main file sample

---
+++ name_of_ur_project/main.py
@@ -0,0 +1,54 @@
+import pygame as pg
+import random as rand
+import math
+import sys
+
+# global references
+try:
+    WIDTH, HEIGHT = int(sys.argv[1]), int(sys.argv[2])
+except IndexError:
+    WIDTH, HEIGHT = 1280, 720
+TICK_RATE = 5
+
+if __name__ == "__main__":
+    # creating a pygame window
+    screen = pg.display.set_mode((WIDTH, HEIGHT), vsync=1)
+    clock = pg.time.Clock()
+
+    # creating the game logic / environment sample
+
+    upd_ticks = pg.time.get_ticks()
+
+    # main cycle
+    while True:
+        # surfaces cleaning
+        screen.fill((140, 140, 140))
+        
+        # checking for keyboard, window, mouse inputs or events
+        keys = pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_u:
+                    TICK_RATE -= 1
+                    upd_ticks = pg.time.get_ticks()
+                if event.key == pg.K_i:
+                    TICK_RATE += 1
+                    upd_ticks = pg.time.get_ticks()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    for btn in btns:
+                        btn.coll([pg.mouse.get_pos()[0] - WIDTH + vx, pg.mouse.get_pos()[1]])
+
+        # game Assets/UI/elements drawing
+
+        # game environment updating
+        if 0 <= pg.time.get_ticks() - upd_ticks - (1000 / TICK_RATE) <= 75:
+            upd_ticks = pg.time.get_ticks()
+            # calling for game environment to update
+
+        pg.display.set_caption("$~Wumpus ~fps: " + str(round(clock.get_fps(), 2)) + " ~tickrate: " + str(TICK_RATE))
+
+        pg.display.flip()
+        clock.tick()
