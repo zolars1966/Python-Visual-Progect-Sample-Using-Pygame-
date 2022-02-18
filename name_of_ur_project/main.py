7ad243572592215da501f7d49d42bb90c1780e07
Arseny Zolotarev <70763346+ckreesher@users.noreply.github.com>
2022-02-18 09:22:48 +0800

    Update main.py

--- name_of_ur_project/main.py
+++ name_of_ur_project/main.py
@@ -3,12 +3,38 @@ import random as rand
 import math
 import sys
 
+
+def left_click():
+    global l_press
+    if event.type == pg.MOUSEBUTTONDOWN:
+        if event.button == 1 and not l_press:
+            l_press = True
+            return True
+    elif event.type == pg.MOUSEBUTTONUP:
+        if event.button == 1:
+            l_press = False
+    return False
+
+
+def right_click():
+    global r_press
+    if event.type == pg.MOUSEBUTTONDOWN:
+        if event.button == 3 and not r_press:
+            r_press = True
+            return True
+    elif event.type == pg.MOUSEBUTTONUP:
+        if event.button == 3:
+            r_press = False
+    return False
+
+
 # global references
 try:
     WIDTH, HEIGHT = int(sys.argv[1]), int(sys.argv[2])
 except IndexError:
     WIDTH, HEIGHT = 1280, 720
 TICK_RATE = 5
+l_press, r_press = False, False
 
 if __name__ == "__main__":
     # creating a pygame window
@@ -36,6 +62,14 @@ if __name__ == "__main__":
                 if event.key == pg.K_i:
                     TICK_RATE += 1
                     upd_ticks = pg.time.get_ticks()
+        
+        if left_click():
+            # do something
+            pass
+        
+        if right_click():
+            # do something
+            pass
 
         # game Assets/UI/elements drawing
 
@@ -44,7 +78,7 @@ if __name__ == "__main__":
             upd_ticks = pg.time.get_ticks()
             # calling for game environment to update
 
-        pg.display.set_caption("$~Wumpus ~fps: " + str(round(clock.get_fps(), 2)) + " ~tickrate: " + str(TICK_RATE))
+        pg.display.set_caption("$~NameOfUrProject ~fps: " + str(round(clock.get_fps(), 2)) + " ~tickrate: " + str(TICK_RATE))
 
         pg.display.flip()
         clock.tick()
